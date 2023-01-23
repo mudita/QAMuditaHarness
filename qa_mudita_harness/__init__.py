2a70ab9bf8c3be7b442011b7ffcc4c477388a1ce
jurgenwigg <jurgenwigg@protonmail.com>
2023-01-23 19:49:22 +0100

    initial commit

--- qa_mudita_harness/__init__.py
+++ qa_mudita_harness/__init__.py
@@ -1,5 +1,6 @@
-"""Top-level package for QAMuditaHarness."""
+# Copyright (c) 2017-2020, Mudita Sp. z.o.o. All rights reserved.
+# For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
+import logging
 
-__author__ = """Jurgen Wigg"""
-__email__ = 'jurgenwigg@protonmail.com'
-__version__ = '0.1.0'
+logging.basicConfig(format='%(asctime)s %(levelname)-6s %(module)-8s:%(filename)s:%(lineno)s %(message)s', level=logging.DEBUG, datefmt="%Y/%m/%d %H:%M:%S")
+log = logging.getLogger(__name__)
